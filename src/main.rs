cde98aba4979eb6c7826d26091fbdf2f53b9c5c4
Victor Rodriguez <awtgerry@gmail.com>
2024-05-15 15:12:35 -0600

    buttons and icons
    clean up rest of files

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
-use iced::widget::{column, row, container, text_editor, text, button};
+use iced::widget::{
+    column,
+    row,
+    container,
+    text_editor,
+    text,
+    button,
+    horizontal_space,
+};
+use iced::theme::Button;
 use iced::Application;
-use iced::{Theme, Command};
+use iced::{Theme, Command, Settings, Element, Font};
 
 fn main() -> iced::Result {
-    TodoApp::run(iced::Settings::default())
+    TodoApp::run(Settings {
+        fonts: vec![include_bytes!("../fonts/icon-font.ttf")
+            .as_slice()
+            .into()],
+        ..Settings::default()
+    })
 }
 
-#[allow(unused)]
 struct TodoApp {
     content: iced::widget::text_editor::Content,
-    error: Option<std::io::Error>,
 }
 
 #[derive(Debug, Clone)]
 enum Todo {
     Edit(iced::widget::text_editor::Action),
-    Register
+    Calendar,
+    Reminder,
+    New
 }
 
 impl Application for TodoApp {
@@ -28,8 +42,7 @@ impl Application for TodoApp {
     fn new(_flags: Self::Flags) -> (Self, Command<Todo>) {
         (
             Self {
-                content: text_editor::Content::new(),
-                error: None,
+                content: text_editor::Content::new()
             },
             Command::none()
         )
@@ -45,7 +58,13 @@ impl Application for TodoApp {
                 self.content.perform(action);
                 Command::none()
             }
-            Todo::Register => {
+            Todo::Calendar => {
+                Command::none()
+            }
+            Todo::Reminder => {
+                Command::none()
+            }
+            Todo::New => {
                 let todo = self.content.text();
                 println!("Todo: {}", todo);
                 Command::none()
@@ -53,19 +72,48 @@ impl Application for TodoApp {
         }
     }
 
-    fn view(&self) -> iced::Element<'_, Self::Message> {
-        let todos_title = text("New todo".to_string());
-        let input = text_editor(&self.content).on_action(Todo::Edit);
-        let register_btn = button("create todo").on_press(Todo::Register);
+    fn view(&self) -> Element<'_, Self::Message> {
+        let todos_title = text("New todo".to_string()).size(25).height(50);
+
+        let new_todo = {
+            let input = text_editor(&self.content).on_action(Todo::Edit).padding(10);
+            column![
+                input,
+                row![
+                    button(calendar_icon()).on_press(Todo::Calendar)
+                        .padding([6, 12])
+                        .style(Button::Text),
+                    button(reminder_icon()).on_press(Todo::Reminder)
+                        .padding([6, 12])
+                        .style(Button::Text),
+                    horizontal_space(),
+                    button("Add").on_press(Todo::New).padding([6, 12]),
+                ].spacing(20)
+            ].spacing(10)
+        };
+
         container(
             column![
                 todos_title,
-                row![input, register_btn]
+                new_todo,
             ],
-        ).padding(10).into()
+        ).padding(20).into()
     }
 
     fn theme(&self) -> Theme {
-        Theme::Dark
+        Theme::TokyoNightLight
     }
+}
+
+fn calendar_icon<'a>() -> Element<'a, Todo> {
+    icon('\u{f133}')
+}
+
+fn reminder_icon<'a>() -> Element<'a, Todo> {
+    icon('\u{e802}')
+}
+
+fn icon<'a>(codepoint: char) -> Element<'a, Todo> {
+    const ICON_FONT: Font = Font::with_name("icon-font");
+    text(codepoint).font(ICON_FONT).into()
 }
