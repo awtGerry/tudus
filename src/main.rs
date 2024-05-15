5b2594f9fa0bb3792dcdb9b1d6ff8c0d5ba15022
Victor Rodriguez <awtgerry@gmail.com>
2024-05-15 11:41:57 -0600

    still testing

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,71 @@
-use iced::widget::{container, text_editor};
-use iced::Sandbox;
+use iced::widget::{column, row, container, text_editor, text, button};
+use iced::Application;
+use iced::{Theme, Command};
 
 fn main() -> iced::Result {
     TodoApp::run(iced::Settings::default())
 }
 
+#[allow(unused)]
 struct TodoApp {
     content: iced::widget::text_editor::Content,
+    error: Option<std::io::Error>,
 }
 
 #[derive(Debug, Clone)]
-enum Message {
+enum Todo {
     Edit(iced::widget::text_editor::Action),
+    Register
 }
 
-impl Sandbox for TodoApp {
-    type Message = Message;
+impl Application for TodoApp {
+    type Message = Todo; // Messages that can be sent to the app
 
-    fn new() -> Self {
-        Self {
-            content: text_editor::Content::new(),
-        }
+    type Theme = Theme; // Custom theme (use default dark for now)
+    type Executor = iced::executor::Default; // engine to run async tasks
+    type Flags = (); // data passed to the app on startup
+
+    fn new(_flags: Self::Flags) -> (Self, Command<Todo>) {
+        (
+            Self {
+                content: text_editor::Content::new(),
+                error: None,
+            },
+            Command::none()
+        )
     }
 
     fn title(&self) -> String {
         String::from("Todo's App")
     }
 
-    fn update(&mut self, message: Self::Message) {
+    fn update(&mut self, message: Self::Message) -> Command<Todo> {
         match message {
-            Message::Edit(action) => {
-                self.content.perform(action)
+            Todo::Edit(action) => {
+                self.content.perform(action);
+                Command::none()
+            }
+            Todo::Register => {
+                let todo = self.content.text();
+                println!("Todo: {}", todo);
+                Command::none()
             }
         }
     }
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
-        let input = text_editor(&self.content).on_action(Message::Edit);
-        container(input).padding(10).into()
+        let todos_title = text("New todo".to_string());
+        let input = text_editor(&self.content).on_action(Todo::Edit);
+        let register_btn = button("create todo").on_press(Todo::Register);
+        container(
+            column![
+                todos_title,
+                row![input, register_btn]
+            ],
+        ).padding(10).into()
+    }
+
+    fn theme(&self) -> Theme {
+        Theme::Dark
     }
 }
